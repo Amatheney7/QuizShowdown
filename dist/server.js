2ea840bebe8206a21e58ad6cf90cd8f9dd586f0f
rmatheney7 <rmatheney7@gmail.com>
2017-09-22 00:53:22 -0500

    setting up rest api

--- dist/server.js
+++ dist/server.js
@@ -5,4 +5,37 @@ app = express();
 app.use(serveStatic(__dirname));
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log('server started ' + port);
+
+app.post('/signUp', (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+
+    User.findOne({ username: username })
+        .exec(function (err, found) {
+            if (!found) {
+                let user = new User({
+                    username: username,
+                    password: password
+                });
+                user.save((err, success) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Welcome to the Game')
+                        res.redirect('/');
+
+                    }
+
+                })
+            } else {
+
+
+                res.redirect('/signUp');
+
+            }
+        });
+
+});
+
